8639e2b5e172153cc0339b2f6d0e4e3545a46865
Tharukie1996 <tharukiej@gmail.com>
2020-12-09 20:15:38 +0530

    Code refactored

--- src/components/barchart/barchart.tsx
+++ src/components/barchart/barchart.tsx
@@ -16,47 +16,48 @@ export const BarChart = (props: Props) => {
     const data = {
         labels: props.labels,
         datasets: [
-          {
-            label: props.title,
-            backgroundColor: 'rgba(22, 116, 64,0.4)',
-            borderColor: 'rgba(22, 116, 64,1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(22, 116, 64,0.8)',
-            hoverBorderColor: 'rgba(22, 116, 64,1)',
-            data: props.dataList,
-            categoryPercentage: 0.5,
-            barPercentage: 1.0,
-          }
+            {
+                label: props.title,
+                backgroundColor: 'rgba(22, 116, 64,0.4)',
+                borderColor: 'rgba(22, 116, 64,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(22, 116, 64,0.8)',
+                hoverBorderColor: 'rgba(22, 116, 64,1)',
+                data: props.dataList,
+                categoryPercentage: 0.5,
+                barPercentage: 1.0,
+            }
         ]
-      };
+    };
 
     return (
-    <div className="ed-barchart">
-        <HorizontalBar height={200}
-            type='horizontalBar' 
-            data={data}
-            options={{scales: {
-                xAxes: [{
-                  ticks: {
-                    beginAtZero: true,
-                    autoSkip: false
-                  }
-                }],
-                yAxes: [{
-                    ticks: {
-                      beginAtZero: true,
-                      autoSkip: false
+        <div className="ed-barchart">
+            <HorizontalBar height={200}
+                type='horizontalBar'
+                data={data}
+                options={{
+                    scales: {
+                        xAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                autoSkip: false
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                autoSkip: false
+                            }
+                        }],
+                        maintainAspectRatio: false,
+                    },
+                    legend: {
+                        display: true,
+                        position: 'bottom'
                     }
-                  }],
-                maintainAspectRatio: false,
-              },
-              legend: {
-                display: true,
-                position: 'bottom'
-            }
-            }}
+                }}
             >
-        </HorizontalBar>
+            </HorizontalBar>
         </div>
     )
 }
--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -9,7 +9,7 @@ export const Dashboard = () => {
     const [operationalCount, setOperationalCount] = useState(0);
     const [nonOperationalCount, setNonOperationalCount] = useState(0);
     const [data, setData] = useState(null as any);
-    const [label, setLabel] =useState(null as any);
+    const [label, setLabel] = useState(null as any);
 
     const request = axios.create({
         baseURL: EQUIPMENT_SERVICE
@@ -17,16 +17,16 @@ export const Dashboard = () => {
 
     const getEquipmentStats = async () => {
         await request.get(`/equipment/stat`)
-        .then(response => {
-            setNonOperationalCount(response.data.noOfNonOperationalEquip);
-            setOperationalCount(response.data.noOfOperationalEquip);
-            setLabel(response.data.labelList);
-            setData(response.data.dataList);
-        });
+            .then(response => {
+                setNonOperationalCount(response.data.noOfNonOperationalEquip);
+                setOperationalCount(response.data.noOfOperationalEquip);
+                setLabel(response.data.labelList);
+                setData(response.data.dataList);
+            });
     }
     const getBarChart = () => {
         let x = data
-        return(
+        return (
             <BarChart dataList={data} labels={label} title="Assets"></BarChart>
         )
     }
@@ -39,12 +39,12 @@ export const Dashboard = () => {
 
     return (
         <>
-        <Header></Header>
-        <div className="ed-desc-p">
-            <p>Operational equipment count: {operationalCount}</p>
-            <p>Non-operational equipment count: {nonOperationalCount}</p>
-        </div>
-        <BarChart dataList={data} labels={label} title="Equipment Count"></BarChart>
+            <Header></Header>
+            <div className="ed-desc-p">
+                <p>Operational equipment count: {operationalCount}</p>
+                <p>Non-operational equipment count: {nonOperationalCount}</p>
+            </div>
+            <BarChart dataList={data} labels={label} title="Equipment Count"></BarChart>
         </>
     )
 }
